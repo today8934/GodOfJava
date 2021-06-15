91b1182d6199a09b41bb0a7b987e952f48b0899e
today8934 <today8934@gmail.com>
2021-06-15 21:38:11 +0900

    챕터2

---
+++ src/Chapter02/Profile.java
@@ -0,0 +1,10 @@
+package Chapter02;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name="OOO";
+        int age = 0;
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+    }
+}
