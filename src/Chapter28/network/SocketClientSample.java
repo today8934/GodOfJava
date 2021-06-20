1e2b3e086fad911bb023c6519710d7fb2f011931
today8934 <today8934@gmail.com>
2021-06-20 19:38:12 +0900

    28장실습 커밋

---
+++ src/Chapter28/network/SocketClientSample.java
@@ -0,0 +1,45 @@
+package Chapter28.network;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class SocketClientSample {
+
+    public static void main(String[] args) {
+        SocketClientSample sample = new SocketClientSample();
+        sample.sendSocketSample();
+    }
+
+    public void sendSocketSample() {
+        for(int loop = 0; loop < 3; loop++) {
+            sendAndReceiveSocketData();
+        }
+    }
+
+    public void sendAndReceiveSocketData() {
+        Socket socket = null;
+        try {
+            System.out.println("client:Connection");
+            socket = new Socket("127.0.0.1", 9999);
+            System.out.println("Client:Connect status=" + socket.isConnected());
+            Thread.sleep(1000);
+            byte[] readByte = new byte[256];
+            InputStream stream = socket.getInputStream();
+            BufferedInputStream in = new BufferedInputStream(stream);
+            in.read(readByte);
+            System.out.println("Client:received data=" + new String(readByte).trim());
+            in.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(socket != null) {
+                try {
+                    socket.close();
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
